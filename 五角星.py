56440e41e9f964e24741d4058aaacf8b285bbcf8
osbing <591590339@qq.com>
2018-11-19 20:11:55 +0800

    编辑格式
    编辑格式

--- 五角星.py
+++ 五角星.py
@@ -8,8 +8,8 @@ turtle.fillcolor("red")
  
 turtle.begin_fill()
 for _ in range(5):
-  turtle.forward(200)
-  turtle.right(144)
+    turtle.forward(200)
+    turtle.right(144)
 turtle.end_fill()
 time.sleep(2)
  
