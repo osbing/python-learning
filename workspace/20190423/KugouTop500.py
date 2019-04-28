1af7f8ab85050bdceed4b342693007b94a4ed93d
osbing <591590339@qq.com>
2019-04-28 22:41:41 +0800

    Update KugouTop500.py

--- workspace/20190423/KugouTop500.py
+++ workspace/20190423/KugouTop500.py
@@ -12,19 +12,24 @@ def get_info(url):
     ranks = soup.select('#rankWrap > div.pc_temp_songlist > ul > li > span.pc_temp_num')
     titles = soup.select('#rankWrap > div.pc_temp_songlist > ul > li > a')
     times = soup.select('#rankWrap > div.pc_temp_songlist > ul > li > span.pc_temp_tips_r > span')
-    for rank,title,time in zip(ranks,titles,times):
+    links = soup.select('#rankWrap > div.pc_temp_songlist > ul > li > a')
+
+    # print(type(links))
+    for rank,title,time,link in zip(ranks,titles,times,links):
         data = {
             'rank':rank.get_text().strip(),
-            'singer':title.get_text().split(' - ')[0],
-            'song':title.get_text().split(' - ')[-1],
-            'time':time.get_text().strip()
+            'singer':title.get_text().strip().split(' - ')[0],
+            'song':title.get_text().strip().split(' - ')[-1],
+            'time':time.get_text().strip(),
+            'link':link.get('href') # 获取 Tag <a /a> 的属性
         }
         print(data)
 
 if __name__ == '__main__':
+    # 酷狗TOP500_排行榜_乐库频道_酷狗网
     urls = ['https://www.kugou.com/yy/rank/home/{}-8888.html'.format(str(i))
             for i in range(1,24)
     ]
     for url in urls:
         get_info(url)
-    time.sleep(1)
+        time.sleep(1)
