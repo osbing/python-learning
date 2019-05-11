821919ad5a93a40c01d6f94b3d3a527eb1986b9e
osbing <591590339@qq.com>
2019-05-11 15:17:26 +0800

    update

--- workspace/20190423/douban.py
+++ workspace/20190423/douban.py
@@ -10,11 +10,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36'
 }
 
-with open('C:/GitHub/python-learning/workspace/20190423/doubanbook.csv','wt',newline='',encoding='utf-8') as fp:
-    writer = csv.writer(fp)
-    writer.writerow(('name','url','author','publisher','date','price','rate','comment')) # 写入header
-
-def get_info(url):
+def get_info(url,file_path):
     html = requests.get(url,headers=headers)
     selector = etree.HTML(html.text)
     # //*[@id="content"]/div/div[1]/div/table[2]/tbody/tr/td[2]/div[1]/a
@@ -31,10 +27,10 @@ def get_info(url):
         rate = info.xpath('td/div/span[2]/text()')[0].strip(' ')
         comments = info.xpath('td/p/span/text()')
         comment = comments[0].strip(' ') if len(comments) != 0 else '空'
-        print(name,url,author,publisher,date,price,rate,comment)
-        with open('C:/GitHub/python-learning/workspace/20190423/doubanbook.csv','a+',newline='',encoding='utf-8') as fp:            
+        print(name,author,publisher,date,price,rate,comment,url)
+        with open(filename_path,'a+',newline='',encoding='utf-8') as fp:            
             writer = csv.writer(fp)
-            writer.writerow((name,url,author,publisher,date,price,rate,comment)) # 写入数据
+            writer.writerow((name,author,publisher,date,price,rate,comment,url)) # 写入数据
 
 # print(infos)
 # print(html) # <Element html at 0x27fdf70c908>
@@ -45,6 +41,10 @@ def get_info(url):
 
 if __name__ == '__main__':
     urls = ['https://book.douban.com/top250?start={}'.format(str(i)) for i in range(0,250,25)] # range(1,12)
+    filename_path = 'C:/GitHub/python-learning/workspace/20190423/doubanbook.csv'
+    with open(filename_path,'wt',newline='',encoding='utf-8') as fp:
+        writer = csv.writer(fp)
+        writer.writerow(('name','author','publisher','date','price','rate','comment','url')) # 写入header
     for url in urls:
-        print(url)
-        get_info(url)
+        # print(url)
+        get_info(url,filename_path)
